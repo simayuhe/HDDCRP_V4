b4f779ee2236f476ff0ee8aaaa10461cae66cb05
simayuhe <1281139679@qq.com>
2017-09-20 17:17:49 +0800

    just for fiun

--- AddFileToGit.sh
+++ AddFileToGit.sh
@@ -4,7 +4,8 @@
 #History:
 #20170920
 git add .
-git commit -m "my test for sh"
+read -p "your commit :" commitstring
+git commit -m " $commitstring "
 git remote add origin git@github.com:simayuhe/HDDCRP_V4.git
 git push -u origin master
 exit 0
---
+++ FetchFileFromGit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#program:
+#	this program help you to Fetch file from git 
+#History:
+#20170920
+git fetch origin master
+git merge origin/master
+exit 0
